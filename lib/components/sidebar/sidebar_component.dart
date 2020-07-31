dac604779daf658b131a324b633354c1948c56f6
Miha Hotko <miha.hotko@lucis.si>
2020-07-31 15:21:39 +0200

    fixed sidebar not showing route on refresh

--- lib/components/sidebar/sidebar_component.dart
+++ lib/components/sidebar/sidebar_component.dart
@@ -1,5 +1,8 @@
+import 'dart:html';
+
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
+
 import 'package:visualizer/animation/anime.dart';
 import 'package:visualizer/helper/get_sidebar_array.dart';
 import 'package:visualizer/models/SidebarContent.dart';
@@ -9,13 +12,25 @@ import 'package:visualizer/models/SidebarContent.dart';
     templateUrl: 'sidebar_component.html',
     styleUrls: ['sidebar_component.css'],
     directives: [coreDirectives, routerDirectives])
-class SidebarComponent {
+class SidebarComponent implements AfterViewChecked {
   List<SidebarContent> list = SidebarArray.arr();
 
   void sidebarClick(dynamic event) {
+    animate(event);
+  }
+
+  @override
+  void ngAfterViewChecked() {
+    var t = querySelector('.block.active');
+    if (t != null) {
+      animate(int.parse(t.id));
+    }
+  }
+
+  void animate(num order) {
     anime(AnimeOptions(
         targets: '.slider',
-        top: event * 60 + 100,
+        top: order * 60 + 100,
         duration: 600,
         easing: 'easeOutExpo'));
   }
